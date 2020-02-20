c1f65a3f162bbc4c19a15eb71dc80e6279c4d5a6
yxeGithub <dvgrimard@gmail.com>
2020-02-20 15:07:45 -0600

    updated to get RTD working properly

--- containthedocs-build.sh
+++ containthedocs-build.sh
@@ -6,7 +6,7 @@ COMMAND="make -C docs html"
 
 . ./containthedocs-image
 
-exec sudo docker run --rm -it \
+exec docker run --rm -it \
   -v "$PWD":"$PWD" --workdir "$PWD" \
   ${DOCKER_RUN_ARGS} \
   -e "LOCAL_USER_ID=$(id -u)" \
